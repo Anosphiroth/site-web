7968936475ba08b84dcec08933790c5e4bdb5428
Nathanaël Bayle <nathanael.bayle0@gmail.com>
2020-05-03 14:07:37 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,1 @@
-function scrollNav() {
-  $('.sidenav a').click(function(){
-    $(".active").removeClass("active");
-    $(this).closest('li').addClass("active");
-    var theClass = $(this).attr("class");
-    $('.'+theClass).parent('li').addClass('active');
-    $('html, body').stop().animate({
-        scrollTop: $( $(this).attr('href') ).offset().top }, 700);
-    return false;
-  });
-  $('.scrollTop a').scrollTop();
-}
-scrollNav();
+.
